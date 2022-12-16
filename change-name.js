a2c82473cc001233f07b354d6c01e74048429ac9
Miho <2300206764@qq.com>
2022-12-16 11:31:46 +0000

    Thanks Evgeniy Jsopn. Now package can publish to sms-activate.

---
+++ change-name.js
@@ -0,0 +1,9 @@
+/* only for sms-activate package publish script */
+/* DONT USE IT IN ANY PRODUCTION */
+
+const fs = require('fs')
+const package = JSON.parse(fs.readFileSync('./package.json'))
+package.name = 'sms-activate'
+
+fs.writeFileSync('./package.json',JSON.stringify(package,null,2))
+console.log('Change Done')
